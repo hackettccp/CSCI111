499902d58ede6f7be109d1f4f2b800e7f542c1dc
Michael Hackett <34200282+hackettccp@users.noreply.github.com>
2025-05-15 10:29:35 -0400

    Bulk removal

--- Modules/Module8/src/object_examples/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package object_examples;
-
-/**
- * This class models a rectangle with length and width fields
- */
-class Rectangle {
-    int width;          //Represents the Rectangle's width
-    int length;         //Represents the Rectangle's length
-}
